ee13f2cd35926bcdc6e57b85341ce69cc9a83c73
Jamie Bliss <jamie@ivyleav.es>
2020-05-30 11:55:11 -0400

    test_urp: stop server in fixture

--- tests/test_urp.py
+++ tests/test_urp.py
@@ -56,101 +56,81 @@ async def linked_pair(echo_service):
     csock, ssock = socket.socketpair()
     server_task = asyncio.create_task(echo_service.serve_inherited_socket(ssock))
     client = await client_from_inherited_socket(csock)
-    return client, server_task
+    yield client, server_task
+    server_task.cancel()
 
 
 @pytest.mark.asyncio
 async def test_basic_roundtrip(linked_pair):
     client, stask = linked_pair
-    try:
-        async with client:
-            async for i, result in aenumerate(client['example.Echo'](spam='eggs')):
-                assert i == 0
-                assert result == {'spam': 'eggs'}
-    finally:
-        stask.cancel()
+    async with client:
+        async for i, result in aenumerate(client['example.Echo'](spam='eggs')):
+            assert i == 0
+            assert result == {'spam': 'eggs'}
 
 
 @pytest.mark.asyncio
 async def test_shoosh(linked_pair):
     client, stask = linked_pair
-    try:
-        async with client:
-            async def _():
-                async for result in client['example.Echo'](spam='eggs'):
-                    pass
-            t = asyncio.create_task(_())
-            t.cancel()
-    finally:
-        stask.cancel()
+    async with client:
+        async def _():
+            async for result in client['example.Echo'](spam='eggs'):
+                pass
+        t = asyncio.create_task(_())
+        t.cancel()
 
 
 @pytest.mark.asyncio
 async def test_sync(linked_pair):
     client, stask = linked_pair
-    try:
-        async with client:
-            async for i, result in aenumerate(client['example.sync']()):
-                assert i == 0
-                assert result == {'spam': 'eggs'}
-    finally:
-        stask.cancel()
+    async with client:
+        async for i, result in aenumerate(client['example.sync']()):
+            assert i == 0
+            assert result == {'spam': 'eggs'}
 
 
 @pytest.mark.asyncio
 async def test_async(linked_pair):
     client, stask = linked_pair
-    try:
-        async with client:
-            async for i, result in aenumerate(client['example.async']()):
-                assert i == 0
-                assert result == {'spam': 'eggs'}
-    finally:
-        stask.cancel()
+    async with client:
+        async for i, result in aenumerate(client['example.async']()):
+            assert i == 0
+            assert result == {'spam': 'eggs'}
 
 
 @pytest.mark.asyncio
 async def test_sync_gen(linked_pair):
     client, stask = linked_pair
-    try:
-        async with client:
-            async for i, result in aenumerate(client['example.gen']()):
-                assert i in (0, 1)
-                if i == 0:
-                    assert result == {'spam': 'eggs'}
-                elif i == 1:
-                    assert result == {'foo': 'bar'}
-                else:
-                    assert False
-    finally:
-        stask.cancel()
+    async with client:
+        async for i, result in aenumerate(client['example.gen']()):
+            assert i in (0, 1)
+            if i == 0:
+                assert result == {'spam': 'eggs'}
+            elif i == 1:
+                assert result == {'foo': 'bar'}
+            else:
+                assert False
 
 
 @pytest.mark.asyncio
 async def test_async_gen(linked_pair):
     client, stask = linked_pair
-    try:
-        async with client:
-            async for i, result in aenumerate(client['example.async_gen']()):
-                assert i in (0, 1)
-                if i == 0:
-                    assert result == {'spam': 'eggs'}
-                elif i == 1:
-                    assert result == {'foo': 'bar'}
-                else:
-                    assert False
-    finally:
-        stask.cancel()
+    async with client:
+        async for i, result in aenumerate(client['example.async_gen']()):
+            assert i in (0, 1)
+            if i == 0:
+                assert result == {'spam': 'eggs'}
+            elif i == 1:
+                assert result == {'foo': 'bar'}
+            else:
+                assert False
 
 
 @pytest.mark.asyncio
 async def test_errors(linked_pair):
     client, stask = linked_pair
-    try:
-        async with client:
-            async for i, result in aenumerate(client['example.error'](msg="spam&eggs")):
-                assert i == 0
-                assert isinstance(result, errors['builtins.Exception'])
-                assert str(result) == "spam&eggs"
-    finally:
-        stask.cancel()
+    async with client:
+        async for i, result in aenumerate(client['example.error'](msg="spam&eggs")):
+            assert i == 0
+            assert isinstance(result, errors['builtins.Exception'])
+            assert str(result) == "spam&eggs"
